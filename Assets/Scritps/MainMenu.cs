c2dc2660b3003cca10636ca68163461271332783
TVD98 <tovuduong98@gmail.com>
2020-07-21 10:59:11 +0700

    first-commit

---
+++ Assets/Scritps/MainMenu.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject findOpponentPanel = null;
+    [SerializeField] private GameObject waitingStatusPanel = null;
+    [SerializeField] private TextMeshProUGUI waitingStatusText = null;
+
+    private bool isConnecting = false;
+
+    private const string GameVersion = "0.1";
+    private const int MaxPlayerPerRoom = 2;
+
+    private void Awake() => PhotonNetwork.AutomaticallySyncScene = true;
+
+    public void FindOpponent()
+    {
+        isConnecting = true;
+
+        findOpponentPanel.SetActive(false);
+        waitingStatusPanel.SetActive(true);
+
+        waitingStatusText.text = "Searching...";
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = GameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conneted to Master");
+
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        findOpponentPanel.SetActive(true);
+        waitingStatusPanel.SetActive(false);
+
+        Debug.Log($"Disconneted due to: {cause}");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No cliens are waitting for an opponent, creating a new room");
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = MaxPlayerPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Client successfully joined a room");
+
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+
+        if(playerCount != MaxPlayerPerRoom)
+        {
+            waitingStatusText.text = "Waiting For Opponent";
+            Debug.Log("Client is waiting for an opponent");
+        }
+        else
+        {
+            waitingStatusText.text = "Opponent Found";
+            Debug.Log("Mach is ready to begin");
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if(PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayerPerRoom)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+
+            waitingStatusText.text = "Opponent Found";
+            Debug.Log("Mach is ready to begin");
+
+            PhotonNetwork.LoadLevel("Scene_Main");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scritps/Test/Main_Test.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Test : MonoBehaviour
+{
+    [SerializeField] private GameObject m_MyGameObject = null;
+
+    private const string m_Scene = "Scene_MainMenu";
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadYourAsyncScene());
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(m_Scene, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(m_MyGameObject, SceneManager.GetSceneByName(m_Scene));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+
+    }
+
+    public static List<T> FindObjectsOfTypeAll<T>()
+    {
+        List<T> results = new List<T>();
+        Debug.Log(SceneManager.sceneCount);
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            var s = SceneManager.GetSceneAt(i);
+            if (s.isLoaded)
+            {
+                var allGameObjects = s.GetRootGameObjects();
+                for (int j = 0; j < allGameObjects.Length; j++)
+                {
+                    var go = allGameObjects[j];
+                    var script = go.GetComponent<T>();
+                    if(script != null)
+                    {
+                        results.Add(script);
+                    }
+
+                }
+            }
+        }
+        return results;
+    }
+}
